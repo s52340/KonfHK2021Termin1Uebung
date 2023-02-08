32bddcaa49b54d125af737545b559bc64b64fec1
bthaler <bianca.thaler@gmx.net>
2023-02-08 02:46:11 +0100

    Klasse Account erstellt, inklusive Konstruktor, Getter + Setter, Methode credit

---
+++ src/Account.java
@@ -0,0 +1,33 @@
+public class Account {
+    private String owner;
+    private double amount;
+
+
+    public Account(String accountOwner) {
+        this.owner = accountOwner;
+        this.amount = 100.000;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    //credit = aufbuchen
+    public void credit(double value){
+        amount = amount + value;
+    }
+
+
+}
